f95cc4c8a5392bc0fb674a50c082fa7e27ffc088
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-04 22:57:04 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './style.css';
-import Route from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Welcome from './Components/Welcome';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
     </div>
   );
 }
