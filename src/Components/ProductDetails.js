a8d71a9992297d7c102a0514c6dd2878ed7efb57
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-04 23:29:13 +0600

    1 file modified

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
 const ProductDetails = () => {
+  const params = useParams();
+
   return (
     <section>
-      <h1> Product Details </h1>
+      <h1> Product Details {params.productId}</h1>
     </section>
   );
 };
