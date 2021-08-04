5d692bb3a9da56120f15e9f1d1a5f23faf343cbb
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-04 23:06:26 +0600

    1 file added

---
+++ src/UI/MainHeader.js
@@ -0,0 +1,17 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/welcome">Welcome</a>
+        </li>
+        <li>
+          <a href="/product">Product</a>
+        </li>
+        <li>
+          <a href="/user">User</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
